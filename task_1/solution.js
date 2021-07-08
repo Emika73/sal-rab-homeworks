d72f053d7381ac2c97e34e08d30eeece343b75a9
Emika73 <polina66693@mail.ru>
2021-07-08 12:02:17 +0300

    tas1_1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -37,7 +37,7 @@ function addInCartCountChange(value) {
     // присвойте ей значение oldValue
     newValue = oldValue;
     // увеличьте значение newValue на 1
-    newValue = oldValue+1;
+    newValue = count + 1;
     // Конец решения задания №1.2.
 
     return newValue;
@@ -50,9 +50,11 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    let newSum = oldSum + difference;
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    let newSumText = newSum + ' ₽';
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    let newSumText = $(newSum)' ₽';
     // Конец решения задания №1.3.
 
     return newSumText;
